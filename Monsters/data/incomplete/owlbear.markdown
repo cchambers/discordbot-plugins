911cfc28413f5f3b5b37ec23b24b584de00477ba
Chris Chambers <cchambers@gmail.com>
2016-03-06 01:34:31 -0500

    first

---
+++ Monsters/data/incomplete/owlbear.markdown
@@ -0,0 +1,49 @@
+### Owlbear
+
+*Large monstrosity, unaligned*
+
+**Armor Class** 13 (natural armor)
+
+**Hit Points** 59 (7d10 + 21)
+
+**Speed** 40 ft.
+
+<table>
+	<thead>
+		<th>STR</th>
+		<th>DEX</th>
+		<th>CON</th>
+		<th>INT</th>
+		<th>WIS</th>
+		<th>CHA</th>
+	</thead>
+	</tbody>
+		<tr>
+			<td>20 (+5)</td>
+			<td>12 (+1)</td>
+			<td>17 (+3)</td>
+			<td>3 (−4)</td>
+			<td>12 (+1)</td>
+			<td>7 (−2)</td>
+		</tr>
+	</tbody>
+</table>
+
+**Skills** Perception +3
+
+**Senses** darkvision 60 ft., passive Perception 13
+
+**Languages** —
+
+**Challenge** 3 (700 XP)
+
+**Keen Sight and Smell.** The owlbear has advantage on Wisdom (Perception) checks that rely on sight or smell.
+
+#### Actions
+
+**Multiattack.** The owlbear makes two attacks: one with its beak and one with its claws.
+
+**Beak.** *Melee Weapon Attack:* +7 to hit, reach 5 ft., one creature. *Hit:* 10 (1d10 + 5) piercing damage.
+
+**Claws.** *Melee Weapon Attack:* +7 to hit, reach 5 ft., one target. *Hit:* 14 (2d8 + 5) slashing damage.
+
---
+++ Monsters/monsters.js
@@ -0,0 +1,108 @@
+var request = require("request");
+var AuthDetails = require("../../auth.json");
+
+var fs = require('fs');
+var path = require('path');
+
+var diretoryTreeToObj = function(dir, done) {
+    var results = [];
+
+    fs.readdir(dir, function(err, list) {
+        if (err)
+            return done(err);
+
+        var pending = list.length;
+
+        if (!pending)
+            return done(null, {name: path.basename(dir), type: 'folder', children: results});
+
+        list.forEach(function(file) {
+            file = path.resolve(dir, file);
+            fs.stat(file, function(err, stat) {
+                if (stat && stat.isDirectory()) {
+                   
+                } else {
+                    var filename = path.basename(file)
+                    results.push(filename.substr(0, filename.length-9).replace(/-/g, " "));
+                    if (!--pending)
+                        done(null, results);
+                }
+            });
+        });
+    });
+};
+
+var spellList;
+var dirTree = ('/home/discordbot/plugins/Spells/data');
+
+diretoryTreeToObj(dirTree, function(err, res){
+    if(err)
+        console.error(err);
+    spellList = res;
+});
+
+function search(array, term) {
+    var results;
+    term = term.toLowerCase();
+    results = array.filter(function(entry) {
+        return entry.toLowerCase().indexOf(term) !== -1;
+    });
+    return results;
+}
+
+
+exports.commands = [
+	"monster", // gives dnd 5e spell info on anything that matches
+    "monsters" // shows data count
+];
+
+exports.monster = {
+	usage: "<search query>",
+	description: "returns monster data",
+	process: function(bot, msg, args) {
+        var results = search(monsterList, args);
+        var others = [];
+        if (results.length != 0 ) {
+            var perfect = results.indexOf( args.toLowerCase() );
+             
+            if (perfect >= 0) {
+                var holdarr = results;
+                var single = results.splice(perfect, 1);
+                others = results;
+                results = single;
+                console.log(results, others);
+            }
+            if (results.length > 1) {
+                bot.sendMessage(msg.channel, "I found **" + results.length + "** monsters matching that term: ```" + results.join(", ") + "```");
+            } else {
+                var file = results[0].replace(/\s/g, "-") + ".markdown";
+                console.log("Trying to pull " + file); 
+                var filename = '/home/discordbot/plugins/Monsters/data/' + file;
+                fs.readFile(filename, 'utf8', function (err, data) {
+                    if (err) throw err;
+                    var edits = data.split("---");
+                    var title = results[0].toUpperCase();
+                    // var tags = edits[1].match(/(?:tags:\s+")(.+)(?:")/g);
+                    var meat = edits[2];
+                    if (others.length > 0) {
+                        bot.sendMessage(msg.channel, "I found **" + others.length + "** other monsters matching that term: ```" + others.join(", ") + "```");
+                    }
+                    setTimeout( function () {
+                         bot.sendMessage(msg.channel, "__**" + title + "**__ " + meat);
+                    },200);
+                });
+            }
+        } else {
+            bot.sendMessage(msg.channel, "I have nothing on that...."); 
+        }
+	}
+}
+
+
+exports.monsters = {
+	usage: "<search query>",
+	description: "returns monster list count",
+	process: function(bot, msg, args) {
+        bot.sendMessage(msg.channel, "I have data on " + spellList.length + " monsters. Search for one with !monster <monstername>"); 
+	}
+}
