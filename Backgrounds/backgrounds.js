aeab0de55ba22673f0bedd3fdcff05529a041e8d
Chris Chambers <cchambers@gmail.com>
2016-06-05 22:00:15 -0400

    grr

--- Backgrounds/backgrounds.js
+++ Backgrounds/backgrounds.js
@@ -16,16 +16,16 @@ var diretoryTreeToObj = function (dir, done) {
         var pending = list.length;
 
         if (!pending)
-            return done(null, {name: path.basename(dir), type: 'folder', children: results});
+            return done(null, { name: path.basename(dir), type: 'folder', children: results });
 
-        list.forEach( function (file) {
+        list.forEach(function (file) {
             file = path.resolve(dir, file);
             fs.stat(file, function (err, stat) {
                 if (stat && stat.isDirectory()) {
-                   
+
                 } else {
                     var filename = path.basename(file)
-                    results.push(filename.substr(0, filename.length-9));
+                    results.push(filename.substr(0, filename.length - 9));
                     if (!--pending)
                         done(null, results);
                 }
@@ -37,8 +37,8 @@ var diretoryTreeToObj = function (dir, done) {
 var dataList;
 var dirTree = ('/home/discordbot/plugins/Backgrounds/data');
 
-diretoryTreeToObj(dirTree, function (err, res){
-    if(err)
+diretoryTreeToObj(dirTree, function (err, res) {
+    if (err)
         console.error(err);
     dataList = res;
 });
@@ -46,7 +46,7 @@ diretoryTreeToObj(dirTree, function (err, res){
 function search(array, term) {
     var results;
     term = term.toLowerCase();
-    results = array.filter( function (entry) {
+    results = array.filter(function (entry) {
         return entry.toLowerCase().indexOf(term) !== -1;
     });
     return results;
@@ -54,67 +54,69 @@ function search(array, term) {
 
 
 exports.commands = [
-	"background", // gives dnd 5e spell info on anything that matches
+    "background", // gives dnd 5e spell info on anything that matches
     "backgrounds" // shows data count
 ];
 
 exports.background = {
-	usage: "<search query> || !mob gob",
-	description: "Returns mob data on whatever ",
-	process: function (bot, msg, args) {
+    usage: "<search query> || !mob gob",
+    description: "Returns mob data on whatever ",
+    process: function (bot, msg, args) {
+
+        function sendMessages(messages, channel) {
+            for (var x = 0; x < messages.length; x++) {
+                var channel = msg.channel;
+                var text = messages[x];
+                var delay = 500 * x;
+                setTimeout( function () {
+                    bot.sendMessage(channel, text);
+                }, delay);
+            }
+        }
+
         var results = search(dataList, args);
         var others = [];
-        if (results.length != 0 ) {
-            var perfect = results.indexOf( args.toLowerCase() );
-             
+        if (results.length != 0) {
+            var perfect = results.indexOf(args.toLowerCase());
+
             if (perfect >= 0) {
                 var single = results.splice(perfect, 1);
                 others = results;
                 results = single;
             }
             if (results.length > 1) {
-                bot.sendMessage(msg.channel, "I found **" + results.length + "** backgrounds matching that term: ```" + results.join (", ") + "```");
+                bot.sendMessage(msg.channel, "I found **" + results.length + "** backgrounds matching that term: ```" + results.join(", ") + "```");
             } else {
                 var file = results[0].replace(/\s/g, "-") + ".markdown";
-                console.log("Trying to pull " + file); 
+                console.log("Trying to pull " + file);
                 var filename = '/home/discordbot/plugins/Backgrounds/data/' + file;
                 fs.readFile(filename, 'utf8', function (err, data) {
-                    setTimeout(function () {
-                        console.log("TEST");
-                    }, 1000)
+
                     if (err) throw err;
                     var meat = data;
                     if (others.length > 0) {
-                        bot.sendMessage(msg.channel, "I found **" + others.length + "** other backgrounds matching that term: ```" + others.join (", ") + "```");
+                        bot.sendMessage(msg.channel, "I found **" + others.length + "** other backgrounds matching that term: ```" + others.join(", ") + "```");
                     }
                     var messages = meat.split("===");
-                    console.log("File length: ", data.length, "Messages: ", messages.length);
-                    for (var x = 0; x < messages.length; x++) {
-                        var channel = msg.channel;
-                        var text = messages[x];
-                        var delay = 200 * x;
-                        // setTimeout( function (channel, text) {
-                            bot.sendMessage(channel, text);
-                        // }, delay);
-                    }
+                    sendMessages(messages, channel);
                 });
             }
         } else {
-            bot.sendMessage(msg.channel, "I have nothing on that..."); 
+            bot.sendMessage(msg.channel, "I have nothing on that...");
         }
-	}
+    }
 }
 
 
 exports.backgrounds = {
-	usage: "",
-	description: "Reloads backgrounds and shows count.",
-	process: function (bot, msg, args) {
-        diretoryTreeToObj(dirTree, function (err, res){
-            if(err)
+    usage: "",
+    description: "Reloads backgrounds and shows count.",
+    process: function (bot, msg, args) {
+        diretoryTreeToObj(dirTree, function (err, res) {
+            if (err)
                 console.error(err);
             dataList = res;
-            bot.sendMessage(msg.channel, "I have data on " + dataList.length + " backgrounds. Search for one with !race <query>"); 
+            bot.sendMessage(msg.channel, "I have data on " + dataList.length + " backgrounds. Search for one with !race <query>");
         });
-	}
+    }
 }
