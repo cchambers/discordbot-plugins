d3ddaa87b63e9058ef93411a0bfd832821a3088b
Chris Chambers <cchambers@gmail.com>
2016-06-08 00:40:54 -0400

    more modern

--- Lexicon/lexicon.js
+++ Lexicon/lexicon.js
@@ -53,7 +53,7 @@ var shelf = {
     },
 
     getTreeData: function (which, where) {
-        diretoryTreeToObj(where, function (err, res) {
+        shelf.diretoryTreeToObj(where, function (err, res) {
             if (err)
                 console.error(err);
             shelf.lexicon[which] = res;
@@ -92,88 +92,6 @@ var shelf = {
 
 shelf.init();
 
-// var directory = __dirname;
-
-// var lexicon = {
-//     backgrounds: [],
-//     classes: [],
-//     conditions: [],
-//     feats: [],
-//     info: [],
-//     monsters: [],
-//     races: [],
-//     shop: [],
-//     spells: []
-// }
-
-// var dataFolder = directory + '/data';
-
-// function diretoryTreeToObj(dir, done) {
-//     var results = [];
-//     fs.readdir(dir, function (err, list) {
-//         if (err)
-//             return done(err);
-
-//         var pending = list.length;
-
-//         if (!pending)
-//             return done(null, { name: path.basename(dir), type: 'folder', children: results });
-
-//         list.forEach(function (file) {
-//             file = path.resolve(dir, file);
-//             fs.stat(file, function (err, stat) {
-//                 if (stat && stat.isDirectory()) {
-
-//                 } else {
-//                     var filename = path.basename(file)
-//                     results.push(filename.substr(0, filename.length - 9));
-//                     if (!--pending)
-//                         done(null, results);
-//                 }
-//             });
-//         });
-//     });
-// };
-
-// function getTreeData(which, where) {
-//     diretoryTreeToObj(where, function (err, res) {
-//         if (err)
-//             console.error(err);
-//         lexicon[which] = res;
-//         console.log(which, lexicon[which].length);
-//     });
-// }
-
-// function search(term, callback) {
-//     var results = [];
-//     for (var array in lexicon) {
-//         types.push(array);
-//         var matches = lexicon[array].filter(function (entry) {
-//             return entry.toLowerCase().indexOf(term) !== -1;
-//         });
-//         console.log("><><>< matches:", matches);
-//         results.push[matches];
-//     }
-//     console.log(types);
-//     return results;
-// }
-// function sendMessages(message, channel, delay) {
-//     setTimeout(function () {
-//         bot.sendMessage(channel, message);
-//     }, delay);
-// }
-
-// function init() {
-//     for (var type in lexicon) {
-//         var where = dataFolder + "/" + type;
-//         getTreeData(type, where);
-//     }
-// }
-
-// function handleSearch(data) {
-//     console.log("Handled:", data);
-// }
-
 exports.commands = [
     "find",
     "finds"
@@ -190,36 +108,7 @@ exports.find = {
         }
 
         var results = shelf.search(term, shelf.handleSearch);
-        // var others = [];
-
-        // if (results.length != 0) {
-        //     var perfect = results.indexOf(args.toLowerCase().replace(/\s/g, "-"));
-        //     if (perfect >= 0) {
-        //         var single = results.splice(perfect, 1);
-        //         others = results;
-        //         results = single;
-        //     }
-
-        //     if (results.length > 1) {
-        //         bot.sendMessage(msg.channel, "I found **" + results.length + "** results matching that term: ```" + results.join(", ") + "```");
-        //     } else {
-        //         var file = results[0].replace(/\s/g, "-") + ".markdown";
-        //         var filename = '/home/discordbot/plugins/Races/data/' + file;
-        //         console.log("Trying to pull " + filename);
-        //         fs.readFile(filename, 'utf8', function (err, data) {
-        //             if (err) throw err;
-        //             var meat = data;
-        //             if (others.length > 0) {
-        //                 bot.sendMessage(msg.channel, "I found **" + others.length + "** other results matching that term: ```" + others.join(", ") + "```");
-        //             }
-        //             setTimeout(function () {
-        //                 bot.sendMessage(msg.channel, meat);
-        //             }, 200);
-        //         });
-        //     }
-        // } else {
-        //     bot.sendMessage(msg.channel, "I have nothing on that...");
-        // }
+    
     }
 }
 
