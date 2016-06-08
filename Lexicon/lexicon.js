412466cfe16485c3cc9a79722b0d454b9cd26be0
Chris Chambers <cchambers@gmail.com>
2016-06-08 00:34:00 -0400

    work foo

--- Lexicon/lexicon.js
+++ Lexicon/lexicon.js
@@ -4,87 +4,179 @@ var AuthDetails = require("../../auth.json");
 var fs = require('fs');
 var path = require('path');
 
-var directory = __dirname;
+var shelf = {
+    directory: __dirname,
+    lexicon: {
+        backgrounds: [],
+        classes: [],
+        conditions: [],
+        feats: [],
+        info: [],
+        monsters: [],
+        races: [],
+        shop: [],
+        spells: []
+    },
+    dataList,
+    dataFolder: shelf.directory + '/data',
+    init: function () {
+        // make this build from the dir structure in the future
+        for (var type in shelf.lexicon) {
+            var where = shelf.dataFolder + "/" + type;
+            shelf.getTreeData(type, where);
+        }
+    },
 
-var lexicon = {
-    backgrounds: [],
-    classes: [],
-    conditions: [],
-    feats: [],
-    info: [],
-    monsters: [],
-    races: [],
-    shop: [],
-    spells: []
-}
+    diretoryTreeToObj: function (dir, done) {
+        var results = [];
+        fs.readdir(dir, function (err, list) {
+            if (err)
+                return done(err);
 
-var types = [];
+            var pending = list.length;
+            if (!pending)
+                return done(null, { name: path.basename(dir), type: 'folder', children: results });
 
-var dataList;
-var dataFolder = directory + '/data';
+            list.forEach(function (file) {
+                file = path.resolve(dir, file);
+                fs.stat(file, function (err, stat) {
+                    if (stat && stat.isDirectory()) {
 
-var diretoryTreeToObj = function (dir, done) {
-    var results = [];
-    fs.readdir(dir, function (err, list) {
-        if (err)
-            return done(err);
-
-        var pending = list.length;
-
-        if (!pending)
-            return done(null, { name: path.basename(dir), type: 'folder', children: results });
-
-        list.forEach(function (file) {
-            file = path.resolve(dir, file);
-            fs.stat(file, function (err, stat) {
-                if (stat && stat.isDirectory()) {
-
-                } else {
-                    var filename = path.basename(file)
-                    results.push(filename.substr(0, filename.length - 9));
-                    if (!--pending)
-                        done(null, results);
-                }
+                    } else {
+                        var filename = path.basename(file)
+                        results.push(filename.substr(0, filename.length - 9));
+                        if (!--pending)
+                            done(null, results);
+                    }
+                });
             });
-        });
-    });
-};
+        })
+    },
 
-function getTreeData(which, where) {
-    diretoryTreeToObj(where, function (err, res) {
-        if (err)
-            console.error(err);
-        lexicon[which] = res;
-        console.log(which, lexicon[which].length);
-    });
-}
-
-function search(term, callback) {
-    var results = [];
-    for (var array in lexicon) {
-        types.push(array);
-        var matches = lexicon[array].filter(function (entry) {
-            return entry.toLowerCase().indexOf(term) !== -1;
+    getTreeData: function (which, where) {
+        diretoryTreeToObj(where, function (err, res) {
+            if (err)
+                console.error(err);
+            shelf.lexicon[which] = res;
+            console.log(which, shelf.lexicon[which].length);
         });
-        console.log("><><>< matches:", matches);
-        results.push[matches];
+    },
+
+    search: function (term, callback) {
+        var results = [];
+        for (var array in shelf.lexicon) {
+            var matches = shelf.lexicon[array].filter(function (entry) {
+                return entry.toLowerCase().indexOf(term) !== -1;
+            });
+            if (matches.length > 0) {
+                console.log(array + " matches:", matches);
+                results.push[matches];
+            }
+        }
+        if (typeof (callback) == "function") {
+            callback(results);
+        } else {
+            return results;
+        }
+    },
+
+    deliver: function (message, channel, delay) {
+        setTimeout(function () {
+            bot.sendMessage(channel, message);
+        }, delay);
+    },
+
+    searchHandler: function (data) {
+        console.log("Handled:", data);
     }
-    console.log(types);
-    return results;
 }
 
-for (var type in lexicon) {
-    var where = dataFolder + "/" + type;
-    getTreeData(type, where);
-}
+shelf.init();
 
-function handleCatSearch () {
+// var directory = __dirname;
 
-}
+// var lexicon = {
+//     backgrounds: [],
+//     classes: [],
+//     conditions: [],
+//     feats: [],
+//     info: [],
+//     monsters: [],
+//     races: [],
+//     shop: [],
+//     spells: []
+// }
 
-function handleSearch (data) {
-    console.log("Handled:", data);
-}
+// var types = [];
+
+// var dataList;
+// var dataFolder = directory + '/data';
+
+// function diretoryTreeToObj(dir, done) {
+//     var results = [];
+//     fs.readdir(dir, function (err, list) {
+//         if (err)
+//             return done(err);
+
+//         var pending = list.length;
+
+//         if (!pending)
+//             return done(null, { name: path.basename(dir), type: 'folder', children: results });
+
+//         list.forEach(function (file) {
+//             file = path.resolve(dir, file);
+//             fs.stat(file, function (err, stat) {
+//                 if (stat && stat.isDirectory()) {
+
+//                 } else {
+//                     var filename = path.basename(file)
+//                     results.push(filename.substr(0, filename.length - 9));
+//                     if (!--pending)
+//                         done(null, results);
+//                 }
+//             });
+//         });
+//     });
+// };
+
+// function getTreeData(which, where) {
+//     diretoryTreeToObj(where, function (err, res) {
+//         if (err)
+//             console.error(err);
+//         lexicon[which] = res;
+//         console.log(which, lexicon[which].length);
+//     });
+// }
+
+// function search(term, callback) {
+//     var results = [];
+//     for (var array in lexicon) {
+//         types.push(array);
+//         var matches = lexicon[array].filter(function (entry) {
+//             return entry.toLowerCase().indexOf(term) !== -1;
+//         });
+//         console.log("><><>< matches:", matches);
+//         results.push[matches];
+//     }
+//     console.log(types);
+//     return results;
+// }
+// function sendMessages(message, channel, delay) {
+//     setTimeout(function () {
+//         bot.sendMessage(channel, message);
+//     }, delay);
+// }
+
+// function init() {
+//     for (var type in lexicon) {
+//         var where = dataFolder + "/" + type;
+//         getTreeData(type, where);
+//     }
+// }
+
+// function handleSearch(data) {
+//     console.log("Handled:", data);
+// }
 
 exports.commands = [
     "find",
@@ -101,7 +193,7 @@ exports.find = {
             return;
         }
 
-        var results = search(term, handleSearch);
+        var results = shelf.search(term, shelf.handleSearch);
         // var others = [];
 
         // if (results.length != 0) {
